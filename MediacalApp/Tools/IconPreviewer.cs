9088c3b514461eadee00291462a945623906d783
Богдан Корницкий <bkornitsky@nntc.pro>
2024-06-29 13:47:06 +0700

    Добавление иконок...

---
+++ MediacalApp/Tools/IconPreviewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace MediacalApp.Tools
+{
+    public sealed class IconPreviewer : UserControl
+    {
+        public static readonly StyledProperty<object?> KeyProperty = AvaloniaProperty.Register<IconPreviewer, object?>("Key");
+
+        public object? Key
+        {
+            get => GetValue(KeyProperty);
+            set => SetValue(KeyProperty, value);
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            if (Name is null)
+            {
+                throw new InvalidOperationException("Name must be set before control is attached to visual tree");
+            }
+
+            var data = this.FindResource(Name);
+            SetCurrentValue(KeyProperty, data);
+            base.OnAttachedToVisualTree(e);
+        }
+    }
+}
