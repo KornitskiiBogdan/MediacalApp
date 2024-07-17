e26d8c232661b1fd848e27f9d6032c601a0ba09e
Богдан Корницкий <bkornitsky@nntc.pro>
2024-07-17 21:48:39 +0700

    Подключил библиотеку для парсера анализов

---
+++ PDFReaderApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using PDFReader;
+
+namespace PDFReaderApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            reader.Read();
+        }
+    }
+}
